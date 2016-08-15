6f8df7df8bba6d0cfe2c7c679ebe0a9f5f5e9164
Scott McCord <scottdmccord@tests-MBP.home>
2016-08-15 10:34:43 -0400

    Added first part of game logic.

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,11 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen"; 
 var cardFour = "queen";
+
+
+if (cardTwo === cardFour) {
+	alert("You found a match!")
+}
+else {
+	alert("Sorry, try again.")
+}
