585dd191e85ea8e2582ddb1547893536f9c29a46
Scott McCord <scottdmccord@tests-MacBook-Pro.local>
2016-08-24 09:00:28 -0400

    Added javascript to create the board.

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,9 +4,23 @@ var cardThree = "queen";
 var cardFour = "queen";
 
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("You found a match!")
 }
 else {
 	alert("Sorry, try again.")
+} */
+
+var board = document.getElementById('game-board');
+
+var x = 4
+
+var createBoard = function() {
+	for (var i = 0; i < x; i += 1) {
+		var cards = document.createElement('div');
+		cards.className = 'card';
+		board.appendChild(cards);
+	}
 }
+
+createBoard()
